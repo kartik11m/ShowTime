1ae7e313f5faaa2510047f532df4b7affcfdecdb
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-22 00:50:20 +0530

    added pages to the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Routes,Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import MovieDetails from './pages/MovieDetails'
+import Movies from './pages/Movies'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favourite from './pages/Favourite'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/movies/:id' element={<MovieDetails/>}/>
+        <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+        <Route path='/favourite' element={<Favourite/>}/>
+      </Routes>
+    </>
   )
 }
 
